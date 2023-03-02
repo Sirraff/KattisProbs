1a847c49f8541498ee12540a80ec12338dd0439e
Raf <76409795+Sirraff@users.noreply.github.com>
2023-03-02 10:44:49 -0800

    Delete peragrams.py

--- peragrams.py
+++
@@ -1,9 +0,0 @@
-def main():
-    word, dict = input(), {}
-    for item in ''.join(set(word)):
-        dict[item] = word.count(item)
-        for key in dict:
-            dict[key] = 1 if (dict[key]%2 != 0) else 0
-    print (sum(x for x in dict.values()) - 1) if sum(x == 1 for x in dict.values())-1 > 1 else print(0)
-if __name__ == "__main__":
-    main()
